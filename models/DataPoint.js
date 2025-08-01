55d2ee316176a242c63abca8a0306b73d9345458
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-01 20:10:35 +0200

    Create DataPoint.js

---
+++ models/DataPoint.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const DataPointSchema = new mongoose.Schema({
+  username:  { type: String, required: true },
+  timestamp: { type: Date,   default: Date.now },
+  location:  { lat: Number, lng: Number, time: Date },
+  sms:       Array,
+  calls:     Array,
+  photo:     String   // base64 da foto, se enviada
+});
+
+module.exports = mongoose.model('DataPoint', DataPointSchema);
