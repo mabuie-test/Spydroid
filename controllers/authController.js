ded0b30802c7930e9fef839b6b391e1f11bb4cd3
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-02 07:02:39 +0200

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+const User = require('../models/User');
+const jwt  = require('jsonwebtoken');
+
+exports.register = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    await new User({ username, password }).save();
+    res.status(201).json({ message: 'Usuário criado' });
+  } catch (e) {
+    if (e.code === 11000) return res.status(409).json({ error: 'Usuário já existe' });
+    next(e);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    res.json({ token, user: { username } });
+  } catch (e) {
+    next(e);
+  }
+};
