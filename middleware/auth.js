e45ca18d1b04bab899563195f00a8e57d93d036a
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-02 07:05:05 +0200

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const h = req.headers.authorization;
+  if (!h || !h.startsWith('Bearer '))
+    return res.status(401).json({ error: 'Token não fornecido' });
+
+  const token = h.split(' ')[1];
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch {
+    res.status(401).json({ error: 'Token inválido' });
+  }
+};
